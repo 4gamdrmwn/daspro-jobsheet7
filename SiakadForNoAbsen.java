b3a43e421412e82485235ecfd76ee41a6a536a43
Agam Darmawan <madridlover085@gmail.com>
2025-11-06 13:35:02 +0700

    modified

--- SiakadForNoAbsen.java
+++ SiakadForNoAbsen.java
@@ -3,6 +3,7 @@ public class SiakadForNoAbsen {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double grade, highest = Double.MIN_VALUE, lowest = Double.MAX_VALUE;
+        int pass = 0, fail = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Enter the grade for student " + i + ":");
@@ -14,10 +15,17 @@ public class SiakadForNoAbsen {
             if (grade < lowest) {
                 lowest = grade;
             }
+            if (grade >= 60) {
+                pass++;
+            } else {
+                fail++;
+            }
         }
 
         System.out.println("The Highest grade: " + highest);
         System.out.println("The Lowest grade: " + lowest);
+        System.out.println("Number of students passed: " + pass);
+        System.out.println("Number of students failed: " + fail);
         
         scanner.close();
     }
