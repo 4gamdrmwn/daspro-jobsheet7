0d91c52a68d42ebd01510f4a13922318a73ccdec
Agam Darmawan <madridlover085@gmail.com>
2025-11-06 13:43:09 +0700

    modified exp 2

--- SiakadWhileNoAbsen.java
+++ SiakadWhileNoAbsen.java
@@ -19,7 +19,6 @@ public class SiakadWhileNoAbsen {
             
             if (grade > 80 && grade <= 100) {
                 System.out.println("The grade of the " + (i + 1) + " student is A");
-                System.out.println("Good, keep the grade!");
             } else if (grade > 73 && grade <= 80) {
                 System.out.println("The grade of the " + (i + 1) + " student is B+");
             } else if (grade > 65 && grade <= 73) {
@@ -32,6 +31,9 @@ public class SiakadWhileNoAbsen {
                 System.out.println("The grade of the " + (i + 1) + " student is D");
             } else {
                 System.out.println("The grade of the " + (i + 1) + " student is E");
+            } if (grade < 0 || grade > 100) {
+                System.out.println("Invalid grade. Re-enter a valid grade!");
+                continue;
             }
             
             i++;
